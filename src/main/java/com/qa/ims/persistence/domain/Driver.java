b0558d6836c558106652856160acf6acbfdddba8
shai-lynch <shai.lynch.sl@gmail.com>
2022-07-21 14:01:03 +0100

    last name update

--- src/main/java/com/qa/ims/persistence/domain/Driver.java
+++ src/main/java/com/qa/ims/persistence/domain/Driver.java
@@ -3,55 +3,46 @@ package com.qa.ims.persistence.domain;
 import java.util.Objects;
 
 public class Driver {
-	
+
 	private Long driverID;
 	private String firstName;
-	private String surname;
+	private String lastName;
 
-
-	public Driver(Long driverID, String firstName, String surname) {
+	public Driver(Long driverID, String firstName, String lastName) {
 		super();
 		this.driverID = driverID;
 		this.firstName = firstName;
-		this.surname = surname;
+		this.lastName = lastName;
 	}
-
 
 	public Long getDriverID() {
 		return driverID;
 	}
 
-
 	public void setDriverID(Long driverID) {
 		this.driverID = driverID;
 	}
-
 
 	public String getFirstName() {
 		return firstName;
 	}
 
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
-
-	public String getSurname() {
-		return surname;
+	public String getLastName() {
+		return lastName;
 	}
 
-
-	public void setSurname(String surname) {
-		this.surname = surname;
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(driverID, firstName, surname);
+		return Objects.hash(driverID, firstName, lastName);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -63,14 +54,12 @@ public class Driver {
 			return false;
 		Driver other = (Driver) obj;
 		return Objects.equals(driverID, other.driverID) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(surname, other.surname);
+				&& Objects.equals(lastName, other.lastName);
 	}
-
 
 	@Override
 	public String toString() {
-		return "Driver [driverID=" + driverID + ", firstName=" + firstName + ", surname=" + surname + "]";
+		return "Driver [driverID=" + driverID + ", firstName=" + firstName + ", surname=" + lastName + "]";
 	}
-	
-	
+
 }
